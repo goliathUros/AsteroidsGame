16b24a29a317c19f58f52240feddbde8eb86de6d
unknown <dayi@697-334-Shelob.sf.k12.internal>
2014-12-03 13:07:52 -0800

    Finally have moving asteroids with the right shape and finally understanding arraylists

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -15,30 +15,36 @@ import java.io.IOException;
 public class AsteroidsGame extends PApplet {
 
 SpaceShip reaper = new SpaceShip();
-Star[] fire = new Star[100];
-Asteroid[] boo = new Asteroid[5];
+Star[] fire = new Star[150];
+ArrayList <Asteroid> meteor = new ArrayList <Asteroid>();
+//Asteroid[] boo = new Asteroid[6];
 
 public void setup() {
-  size(500,500);
+  size(700,700);
+
   for(int i = 0; i < fire.length; i++){
     fire[i] = new Star();
   }
-  for(int i = 0; i < boo.length; i++){
-    boo[i] = new Asteroid();
+
+  for(int i = 0; i <= 6; i++) {
+    meteor.add(i, new Asteroid());
   }
+  /*for(int i = 0; i < boo.length; i++){
+    boo[i] = new Asteroid();
+  }*/
 }
 
 public void keyPressed() {
   if (keyPressed) {
-    if(keyCode == RIGHT){reaper.rotate(10);}
-    if(keyCode == LEFT){reaper.rotate(-10);}
+    if(keyCode == RIGHT){reaper.rotate(15);}
+    if(keyCode == LEFT){reaper.rotate(-15);}
     if(keyCode == UP){reaper.accelerate(0.12f);}
     if(keyCode == DOWN){reaper.accelerate(-0.12f);}
     if(key == ' '){
       reaper.setDirectionX(0);
       reaper.setDirectionY(0);
-      reaper.setCenterX((int)(Math.random()*(500-20))+10);
-      reaper.setCenterY((int)(Math.random()*(500-20))+10);
+      reaper.setCenterX((int)(Math.random()*(700-20))+10);
+      reaper.setCenterY((int)(Math.random()*(700-20))+10);
       reaper.setPointDirection((int)(Math.random()*360));
     }
   }
@@ -51,21 +57,33 @@ public void draw() {
   }
   reaper.show();
   reaper.move();
-  for(int i = 0; i < boo.length; i++){
+
+  for(int i = 0; i < meteor.size(); i++){
+    meteor.get(i).show();
+    meteor.get(i).move();
+  }
+
+  /*for(int i = 0; i < boo.length; i++){
     boo[i].show();
     boo[i].move();
+  }*/
+  for(int i = 0; i < meteor.size(); i++) {
+    if (dist(reaper.getCenterX(), reaper.getCenterY(), (meteor.get(i)).getCenterX(), (meteor.get(i)).getCenterY()) < meteor.get(i).getAstSize()) {
+      meteor.remove(i);
+      meteor.add(new Asteroid());
+    }
   }
 }
 
 class Star {
   private int xPos, yPos;
   public Star() {
-    xPos = (int)(Math.random()*500);
-    yPos = (int)(Math.random()*500);
+    xPos = (int)(Math.random()*700);
+    yPos = (int)(Math.random()*700);
   }
   public void shine(){
-    stroke(255);
-    fill(255);
+    stroke(0, 255, 255);
+    fill(0, 255, 255);
     ellipse(xPos, yPos, 2, 2);
   }
 }
@@ -78,8 +96,8 @@ class SpaceShip extends Floater  {
     xCorners = shipX;
     yCorners = shipY;
     myColor = color(0, 255, 0);
-    myCenterX = (500/2);
-    myCenterY = (500/2);
+    myCenterX = (700/2);
+    myCenterY = (700/2);
     myDirectionX = 0;
     myDirectionY = 0;
     myPointDirection = 0;
@@ -98,20 +116,22 @@ class SpaceShip extends Floater  {
 
 class Asteroid extends Floater {
   private int astSpin;
+  private int astSize;
   Asteroid()
   {
     corners = 8;
-    int[] astX = {20, 5, 0, -5, -20, -5, 0, 5};
-    int[] astY = {0, 5, 20, 5, 0, -5, -20, -5};
+    astSize = ((int)(Math.random()*60)+20);
+    int[] astX = {astSize, (astSize/4), 0, -(astSize/4), -astSize, -(astSize/4), 0, (astSize/4)};
+    int[] astY = {0, (astSize/4), astSize, (astSize/4), 0, -(astSize/4), -astSize, -(astSize/4)};
     xCorners = astX;
     yCorners = astY;
     myColor = color(255);
-    myCenterX = ((int)(Math.random()*500));
-    myCenterY = ((int)(Math.random()*500));
-    myDirectionX = ((int)(Math.random()*5)-2);
-    myDirectionY = ((int)(Math.random()*5)-2);
-    myPointDirection = ((int)(Math.random()*5)-2);
-    astSpin = ((int)(Math.random()*8)-4);
+    myCenterX = ((int)(Math.random()*700));
+    myCenterY = ((int)(Math.random()*700));
+    myDirectionX = ((int)(Math.random()*3)-1);
+    myDirectionY = ((int)(Math.random()*3)-1);
+    myPointDirection = ((int)(Math.random()*360));
+    astSpin = ((int)(Math.random()*7)-3 );
   }
   public void setCenterX(int x) {myCenterX = x;}
   public int getCenterX() {return (int)myCenterX;}
@@ -123,6 +143,7 @@ class Asteroid extends Floater {
   public double getDirectionY() {return myDirectionY;}
   public void setPointDirection(int degrees) {myPointDirection = degrees;}
   public double getPointDirection() {return myPointDirection;}
+  public double getAstSize() {return astSize;}
 
   public void move() 
   {
